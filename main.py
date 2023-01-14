3052c1bc123d56b7feb70c58bf74d0086c294cf6
dclavijo <davis_dsan@hotmail.com>
2023-01-14 12:33:25 -0500

    Empezando con git

---
+++ main.py
@@ -0,0 +1,7 @@
+nombre =input("Ingrese su nombre: ")
+apellido =input("Ingrese su apellido: ")
+edad = input("Ingrese su edad: ")
+
+fullname = "Mi nombre es: " + nombre + "\nMi apellido es: " + apellido + "\nTengo una edad de: " + edad
+
+print(fullname)
